d8bc25a9f3df43940f23b3e3142a6f8f01907e4f
Travis Council <travis@councilstudios.com>
2021-01-28 14:50:34 -0700

    Updates

---
+++ precache-manifest.af8c646b4533eccabfe7c5c9851d7431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acdf081595471d65a8c4b29bd0d04ba6",
+    "url": "/PomodoroTimer/index.html"
+  },
+  {
+    "revision": "f998ce6d988c1abad96c",
+    "url": "/PomodoroTimer/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "226820f6af32e486a61f",
+    "url": "/PomodoroTimer/static/js/2.0885f580.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/PomodoroTimer/static/js/2.0885f580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f998ce6d988c1abad96c",
+    "url": "/PomodoroTimer/static/js/main.379ef4ad.chunk.js"
+  },
+  {
+    "revision": "bff7b314cba3548e6aae",
+    "url": "/PomodoroTimer/static/js/runtime-main.bbcbb8b2.js"
+  }
+]);
